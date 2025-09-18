20a9f2293de3b8e942c4c9feca3804be04b2ac9d
iwindd <freewinendlife@gmail.com>
2025-09-18 19:51:32 +0700

    feat(internationalization): integrate next-intl for localization support in layout and home page

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,3 +1,7 @@
+import { useTranslations } from "next-intl";
+
 export default function Home() {
-  return <div>Hello NextJS</div>;
+  const __ = useTranslations("Home");
+
+  return <div>{__("title")}</div>;
 }
