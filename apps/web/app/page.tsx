461055efc55332afc904ae441fd9b9809031c90b
iwindd <freewinendlife@gmail.com>
2025-09-18 12:26:29 +0700

    refactor(home): simplify homepage by removing Logo and IconChevronDown components, replacing with a basic greeting

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,11 +1,3 @@
-import { IconChevronDown } from "@pawpal/icons/core";
-import Logo from "@pawpal/ui/components/Logo/index";
-
 export default function Home() {
-  return (
-    <div>
-      <Logo />
-      <IconChevronDown />
-    </div>
-  );
+  return <div>Hello NextJS</div>;
 }
