e9f2c0e8607ba0f864b4946f7941fe0fbd1fea53
iwindd <freewinendlife@gmail.com>
2025-09-19 00:11:35 +0700

    refactor(theme): update primary color and shade in theme configuration; adjust Navbar styles for active state

--- apps/web/src/configs/theme.ts
+++ apps/web/src/configs/theme.ts
@@ -10,6 +10,19 @@ const configTheme: MantineThemeOverride = {
 
   /** font-family used in all components, system fonts by default */
   fontFamily: "Sarabun, sans-serif",
+
+  /** Index of theme.colors[color].
+   *  Primary shade is used in all components to determine which color from theme.colors[color] should be used.
+   *  Can be either a number (0–9) or an object to specify different color shades for light and dark color schemes.
+   *  Default value `{ light: 6, dark: 8 }`
+   * */
+  primaryShade: { light: 6, dark: 8 },
+
+  /** Key of `theme.colors`, hex/rgb/hsl values are not supported.
+   *  Determines which color will be used in all components by default.
+   *  Default value – `blue`.
+   * */
+  primaryColor: "pink",
 };
 
 export default configTheme;
