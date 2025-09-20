fde1dac2735c93cb83bf88c53cefad1ee86fd7f8
iwindd <freewinendlife@gmail.com>
2025-09-20 16:57:00 +0700

    feat(api): add ZodValidationPipe for schema validation and include @pawpal/shared dependency

---
+++ apps/api/src/common/ZodValidationPipe.ts
@@ -0,0 +1,16 @@
+import { BadRequestException, Injectable, PipeTransform } from '@nestjs/common';
+import { ZodType } from '@pawpal/shared';
+
+@Injectable()
+export class ZodValidationPipe implements PipeTransform {
+  constructor(private readonly schema: ZodType) {}
+
+  transform(value: any) {
+    console.log(value);
+    const result = this.schema.safeParse(value);
+    if (!result.success) {
+      throw new BadRequestException('validation_failed');
+    }
+    return result.data;
+  }
+}
