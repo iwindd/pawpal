4080b88f25303c1a1dc5010afe0ccc5b39b11eaf
iwindd <freewinendlife@gmail.com>
2025-09-23 01:36:05 +0700

    feat(prisma): implement PrismaService for database connection management; update UserModule and UserService to utilize PrismaService for database operations

--- apps/api/src/modules/user/user.module.ts
+++ apps/api/src/modules/user/user.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
 
 @Module({
   exports: [UserService],
   controllers: [UserController],
-  providers: [UserService],
+  providers: [UserService, PrismaService],
 })
 export class UserModule {}
--- apps/api/src/modules/user/user.service.ts
+++ apps/api/src/modules/user/user.service.ts
@@ -1,12 +1,14 @@
 import authConfig from '@/config/auth';
 import { Injectable } from '@nestjs/common';
-import { prisma, User } from '@pawpal/prisma';
+import { User } from '@pawpal/prisma';
 import { RegisterInput } from '@pawpal/shared';
 import bcrypt from 'bcrypt';
+import { PrismaService } from '../prisma/prisma.service';
 @Injectable()
 export class UserService {
+  constructor(private readonly prisma: PrismaService) {}
   async findByEmail(email: string): Promise<User | null> {
-    return await prisma.user.findUnique({
+    return await this.prisma.user.findUnique({
       where: { email },
     });
   }
@@ -17,7 +19,7 @@ export class UserService {
       authConfig.bcryptSaltRounds,
     );
 
-    return await prisma.user.create({
+    return await this.prisma.user.create({
       data: {
         displayName: user.displayName,
         email: user.email,
