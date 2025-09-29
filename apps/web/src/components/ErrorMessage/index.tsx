daead1967083bfa478277fb24fc156774d12b290
iwindd <freewinendlife@gmail.com>
2025-09-29 13:52:26 +0700

    feat(ErrorMessage): enhance ErrorMessage component with size, disabledAutoFormat, and formatGroup props for improved customization

--- apps/web/src/components/ErrorMessage/index.tsx
+++ apps/web/src/components/ErrorMessage/index.tsx
@@ -4,16 +4,27 @@ import { useTranslations } from "next-intl";
 interface ErrorMessageProps {
   message?: string | null;
   align?: "start" | "center" | "end";
+  size?: "xs" | "sm" | "md" | "lg" | "xl";
+  disabledAutoFormat?: boolean;
+  formatGroup?: string;
 }
 
-const ErrorMessage = ({ message, align = "center" }: ErrorMessageProps) => {
+const ErrorMessage = ({
+  message,
+  align = "center",
+  size = "sm",
+  disabledAutoFormat = false,
+  formatGroup = "",
+}: ErrorMessageProps) => {
   const { colorScheme } = useMantineColorScheme();
   const __ = useTranslations();
   if (!message) return null;
 
+  const formattedMessage = formatGroup ? `${formatGroup}.${message}` : message;
+
   return (
-    <Text size="sm" ta={align} c={colorScheme === "dark" ? "red.8" : "red.6"}>
-      {__(message)}
+    <Text size={size} ta={align} c={colorScheme === "dark" ? "red.8" : "red.6"}>
+      {disabledAutoFormat ? message : __(formattedMessage)}
     </Text>
   );
 };
