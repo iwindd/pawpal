5d8894fdf3c96940b2b269f9976b7e08575499ac
iwindd <freewinendlife@gmail.com>
2025-09-30 15:40:58 +0700

    feat(admin): add dependencies for admin app including icons, prisma, shared components, and various libraries for improved functionality

---
+++ apps/admin/eslint.config.mjs
@@ -0,0 +1,17 @@
+import { nextJsConfig } from "@pawpal/eslint-config/next-js";
+
+const eslintConfig = [
+  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    ignores: [
+      "node_modules/**",
+      ".next/**",
+      "out/**",
+      "build/**",
+      "next-env.d.ts",
+    ],
+  },
+  ...nextJsConfig,
+];
+
+export default eslintConfig;
