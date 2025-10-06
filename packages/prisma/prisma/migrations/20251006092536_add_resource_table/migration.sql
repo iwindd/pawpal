d0c4aceedcf29dc1b2bd339094800f4e022c3010
iwindd <freewinendlife@gmail.com>
2025-10-06 16:25:51 +0700

    feat(resources): enhance file upload functionality with improved validation and integrate UploadImageModal for better resource management

---
+++ packages/prisma/prisma/migrations/20251006092536_add_resource_table/migration.sql
@@ -0,0 +1,17 @@
+-- CreateEnum
+CREATE TYPE "public"."ResourceType" AS ENUM ('IMAGE');
+
+-- CreateTable
+CREATE TABLE "public"."resources" (
+    "id" TEXT NOT NULL,
+    "url" TEXT NOT NULL,
+    "type" "public"."ResourceType" NOT NULL DEFAULT 'IMAGE',
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "user_id" TEXT NOT NULL,
+
+    CONSTRAINT "resources_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "public"."resources" ADD CONSTRAINT "resources_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "public"."users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
