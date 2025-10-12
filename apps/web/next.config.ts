83027cb14040439691a2d8b3c76d0da6e83454db
iwindd <freewinendlife@gmail.com>
2025-10-13 01:59:59 +0700

    feat(resource-image): create ResourceImage component for handling images with dynamic storage URL

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -1,6 +1,8 @@
 import type { NextConfig } from "next";
 import createNextIntlPlugin from "next-intl/plugin";
 
+const STORAGE_URL = new URL(process.env.NEXT_PUBLIC_STORAGE_URL || "");
+
 const nextConfig: NextConfig = {
   images: {
     qualities: [25, 50, 75, 100],
@@ -10,6 +12,12 @@ const nextConfig: NextConfig = {
         hostname: "cdn.gtranslate.net",
         port: "",
         pathname: "/flags/svg/**",
+      },
+      {
+        protocol: STORAGE_URL.protocol === "http:" ? "http" : "https",
+        hostname: STORAGE_URL.hostname,
+        port: STORAGE_URL.port || "8000",
+        pathname: `${STORAGE_URL.pathname}/**`,
       },
     ],
   },
